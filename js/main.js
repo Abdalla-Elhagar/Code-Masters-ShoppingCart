f0b227e169f7acc2bc397f5f693a771049675daa
Abdalla-Elhagar <134546609+Abdalla-Elhagar@users.noreply.github.com>
2024-10-17 21:16:38 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const home = document.querySelector(".home"),
 logIn = document.querySelector(".login");
 
 window.addEventListener("load", () => {
-    if(JSON.parse(localStorage.getItem("loginCheck")) == false) {
+    if(JSON.parse(localStorage.getItem("loginCheck")) == false |JSON.parse(localStorage.getItem("loginCheck")) == undefined ) {
     window.location.href = "pages/login.html"
 }
 })
